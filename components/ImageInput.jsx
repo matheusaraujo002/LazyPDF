7520c7426a2fae92dab2b70373f39ec155093c35
Matheus de Araújo <matheusdearaujo1209@gmail.com>
2024-10-01 17:03:59 -0300

    Melhorando o prompt

--- components/ImageInput.jsx
+++ components/ImageInput.jsx
@@ -27,7 +27,7 @@ export default function ImageInput({ onStartLoading }) {
           const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GOOGLE_API_KEY);
           const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
           const imageBase64 = reader.result.split(',')[1];
-          const prompt = "Você recebeu uma imagem nesse prompt? Se sim, escreva tudo que tiver de texto nessa imagem.";
+          const prompt = "Você recebeu uma imagem nesse prompt? Se sim, escreva tudo que tiver de texto nessa imagem. Não adicione icones ou imagem nesse texto gerado. Entregue o texto em um formato adequado para PDF.";
           const imageParts = [
             {
               inlineData: {
@@ -40,7 +40,6 @@ export default function ImageInput({ onStartLoading }) {
           const result = await model.generateContent([prompt, ...imageParts]);
           const response = await result.response;
           const text = response.text();
-          console.log(text); 
 
           // Armazena o texto na sessão
           sessionStorage.setItem('extractedText', text);
